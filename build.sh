24db0343089ed7c51aa964b3d6291fd8e219f35c
Phumin Maliwan <32133224+mantvmass@users.noreply.github.com>
2021-10-11 23:25:15 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ cd ~/../usr/etc/apt
 rm -rf sources.list.d
 cd
 apt-get update -y
-pkg install python -y
 pkg install proot-distro -y
 cd os-installer
 chmod +x os-installer.sh
 mv os-installer.sh ../
+pkg install python -y
 python os-installer.py
