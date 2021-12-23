c8bf42bd0354a81d5d4df2f6472ced6efcba4c31
Phumin Maliwan <32133224+mantvmass@users.noreply.github.com>
2021-12-23 17:55:06 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+cd os-installer
+chmod +x os-installer
+
+
 mv os-install ../../usr/etc
 mv os-installer ../../usr/bin
+
+
 cd ..
 cd ../usr/etc/apt
 rm -rf sources.list.d
 cd
+
+
+apt install nano -y
 apt-get update -y
 apt-get upgrade -y
 apt upgrade -y
